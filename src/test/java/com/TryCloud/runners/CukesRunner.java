3fd112dd123b7f958e893580429731fb4a26414f
Mayira <mahira223@hotmail.com>
2022-11-10 21:57:02 -0500

    runner class created

---
+++ src/test/java/com/TryCloud/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.TryCloud.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/TryCloud/step_definitions",
+        dryRun = false,
+        tags = "@us04"
+)
+
+public class CukesRunner {
+
+}
